e91f55cf2caa607c7c727a2c49064c68dbcc12df
Pritesh <pptsingh123@gmail.com>
2026-06-23 04:18:23 +0530

    fix : replaced with.error to body.error

--- src/main/java/com/nascorp/marketpal/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nascorp/marketpal/exception/GlobalExceptionHandler.java
@@ -1,12 +1,13 @@
 package com.nascorp.marketpal.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,7 +16,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
         Map<String, String> error = new HashMap<>();
         error.put("error", ex.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).with(error);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     
