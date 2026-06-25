9afb87775ede6742167152468d8d848e1b3855e9
Pritesh <pptsingh123@gmail.com>
2026-06-25 21:58:17 +0530

    chore : test init jwt

--- src/main/java/com/nascorp/marketpal/controller/AuthController.java
+++ src/main/java/com/nascorp/marketpal/controller/AuthController.java
@@ -1,16 +1,17 @@
 package com.nascorp.marketpal.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.nascorp.marketpal.dto.AuthResponse;
 import com.nascorp.marketpal.dto.LoginRequest;
 import com.nascorp.marketpal.dto.RegisterRequest;
 import com.nascorp.marketpal.service.AuthService;
-
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -28,6 +29,15 @@ public class AuthController {
     (@Valid @ RequestBody RegisterRequest request) { 
         String message = authService.register(request);
         return ResponseEntity.ok(message);
+    }
+
+    @GetMapping("/test-protected")
+    public ResponseEntity<?> testProtected(HttpServletRequest request) {
+        
+        String username = SecurityContextHolder.getContext()
+                          .getAuthentication()
+                          .getName();
+        return ResponseEntity.ok(Map.of("message", "You are authenticated!", "username", username));
     }
 
     @PostMapping("/login")
